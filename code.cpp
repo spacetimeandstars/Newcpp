d766c22465522f5f529741ee3f19a3983f38f5e7
aman tiwari <amnntiwari10@gmail.com>
2026-02-04 15:46:29 +0530

    add new line

--- code.cpp
+++ code.cpp
@@ -5,5 +5,6 @@ int main() {
     cout << "Hello, AmanTiwari!" << endl;
      cout << "Hello, World!" << endl;
      cout << "Hello, Good Afternoon!" << endl;
+     cout << "Hello, Everyone!" << endl;
     return 0;
 }
